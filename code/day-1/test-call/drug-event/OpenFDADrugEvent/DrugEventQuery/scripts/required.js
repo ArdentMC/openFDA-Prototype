9534b8ac209cc91e2ec8e05f381639e06a1c7296
Eugene T. Staten II <frontrunner148@hotmail.com>
2015-06-20 04:13:52 -1000

    Food and Drug api tester

---
+++ code/day-1/test-call/drug-event/OpenFDADrugEvent/DrugEventQuery/scripts/required.js
@@ -0,0 +1,12 @@
+﻿var requiredModules = [
+    'esri/map',
+    'esri/layers/GraphicsLayer',
+    'esri/arcgis/utils',
+    'esri/symbols/PictureMarkerSymbol',
+    "esri/renderers/SimpleRenderer",
+    "esri/graphic",
+	  'esri/config',
+	  'esri/dijit/Print',
+	  'esri/geometry/Extent',
+	  'esri/geometry/Point'
+];
