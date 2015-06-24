194026800817d3f6df033bc356b499b122d2efbb
Eugene T. Staten II <frontrunner148@hotmail.com>
2015-06-24 04:01:51 -1000

    Removed food menu and added route from link on home page.

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -3,7 +3,9 @@
 
 angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
+		$scope.isAuth = false;
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		$scope.isAuth = ($scope.authentication.user != null && $scope.authentication.user._id != null);
 	}
 ]);
